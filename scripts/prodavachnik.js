fd97e902dd5c5ae4556fe7d6c726af050ad84366
Spaskich <spaskicha@gmail.com>
2017-06-17 11:45:30 +0300

    Revert "Task 4,1"
    This reverts commit c47125f87f8b3da27f5e03a249d830b1d90c413e.

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,9 +1,4 @@
 function startApp() {
-    if (sessionStorage.getItem('authToken') !== null) {
-        let username = sessionStorage.getItem('username');
-        $('#loggedInUser').text("Welcome, " + username + "!");
-    }
-
     showHideMenuLinks();
     showHomeView();
 
@@ -41,7 +36,6 @@ function startApp() {
             $("#linkListAds").hide();
 			$("#linkCreateAd").hide();
             $("#linkLogout").hide();
-            $("#loggedInUser").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
@@ -49,7 +43,6 @@ function startApp() {
             $("#linkListAds").show();
 			$("#linkCreateAd").show();
             $("#linkLogout").show();
-            $("#loggedInUser").show();
         }
     }
 
@@ -103,9 +96,6 @@ function startApp() {
         sessionStorage.setItem('authToken', userAuth);
         let userId = userInfo._id;
         sessionStorage.setItem('userId', userId);
-        let username = userInfo.username;
-        sessionStorage.setItem('username', username);
-        $('#loggedInUser').text("Welcome, " + username + "!");
     }
 
     // user/register
